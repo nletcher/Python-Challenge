dbf73785fae3dd357ff2a54402d68acd7f01ca78
Sofia Mwaliza Mbega <sofiamwaliza@gmail.com>
2021-02-10 20:49:03 -0800

    Final Submission

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -1,17 +1,14 @@
 import csv
 import os
 
-file_to_load = os.path.join("Resources", "election_data.csv")
-file_to_output = os.path.join("analysis", "election_analysis.txt")
+file_to_load = os.path.join("election_data.csv")
+file_to_output = os.path.join("election_analysis.txt")
 
-# Total Vote Counter
 total_votes = 0
 
-# Candidate Options and Vote Counters
 candidate_options = []
 candidate_votes = {}
 
-# Winning Candidate and Winning Count Tracker
 winning_candidate = ""
 winning_count = 0
 
@@ -46,34 +43,25 @@ with open(file_to_output, "w") as txt_file:
         f"-------------------------\n")
     print(election_results, end="")
 
-    # Save the final vote count to the text file
     txt_file.write(election_results)
 
-    # Determine the winner by looping through the counts
     for candidate in candidate_votes:
 
-        # Retrieve vote count and percentage
         votes = candidate_votes.get(candidate)
         vote_percentage = float(votes) / float(total_votes) * 100
 
-        # Determine winning vote count and candidate
         if (votes > winning_count):
             winning_count = votes
             winning_candidate = candidate
 
-        # Print each candidate's voter count and percentage
         voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
         print(voter_output, end="")
 
-        # Save each candidate's voter count
         txt_file.write(voter_output)
 
-    # Print the winning candidate (to terminal)
     winning_candidate_summary = (
         f"-------------------------\n"
         f"Winner: {winning_candidate}\n"
         f"-------------------------\n")
     print(winning_candidate_summary)
-
-    # Save the winning candidate's name to the text file
     txt_file.write(winning_candidate_summary)
